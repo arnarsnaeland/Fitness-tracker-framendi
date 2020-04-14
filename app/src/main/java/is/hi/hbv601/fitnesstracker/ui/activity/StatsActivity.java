ee27edde4c5a6d728fe419a263bdd48f88a56e73
olgeiringi095 <olgeiringi095@gmail.com>
2020-04-14 23:47:47 +0000

    MainActivity takkar tengdir við viðeigandi activity
    manifest xml skjal einnig uppfært

---
+++ app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/StatsActivity.java
@@ -0,0 +1,16 @@
+package is.hi.hbv601.fitnesstracker.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import is.hi.hbv601.fitnesstracker.R;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
