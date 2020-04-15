55ab253796b39f9f4d6b2fc811bb6da14d3d2984
olgeiringi095 <olgeiringi095@gmail.com>
2020-04-15 14:32:09 +0000

    Addstrength
    og smá fiff

--- app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddCardioActivity.java
+++ app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddCardioActivity.java
@@ -1,12 +1,15 @@
 package is.hi.hbv601.fitnesstracker.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,11 +23,9 @@ public class AddCardioActivity extends AppCompatActivity {
     //private EditText CardioLengthInput;
     //in minutes
     int cardioDuration;
-    private EditText mCardioDuration;
 
     //in km
     double distance;
-    private EditText mDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +38,7 @@ public class AddCardioActivity extends AppCompatActivity {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         CardioSpinner.setAdapter(myAdapter);
 
+        //ÞESSI KÓÐABÚTUR LÆTUR APPIÐ KRASSA AF EINHVERJUM ÁSTÆÐUM
         /*CardioSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -46,6 +48,27 @@ public class AddCardioActivity extends AppCompatActivity {
 
         //cardioLength = Integer.valueOf(CardioLengthInput.getText().toString());
 
+        final Button mSaveCardioButton = findViewById(R.id.save_cardio);
 
+        /**
+         * Button
+         * Navigates to main activity page
+         */
+        mSaveCardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Tengja við gagnagrunn þannig að æfingar saveist
+                Toast.makeText(getBaseContext(), "Saving Exercise...", Toast.LENGTH_SHORT).show();
+
+                MainActivity(v);
+                finish();
+            }
+
+        });
+    }
+
+    public void MainActivity(View v) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
