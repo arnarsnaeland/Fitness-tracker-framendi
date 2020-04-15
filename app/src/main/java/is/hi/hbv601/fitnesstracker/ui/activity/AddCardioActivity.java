ddf622e8158c7a4936035de10fb390e8383e06f3
nth15 <nth15@hi.is>
2020-04-15 23:57:15 +0000

    POstar exercises allt hardcoded samt

--- app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddCardioActivity.java
+++ app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddCardioActivity.java
@@ -1,24 +1,45 @@
 package is.hi.hbv601.fitnesstracker.ui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import is.hi.hbv601.fitnesstracker.R;
+import is.hi.hbv601.fitnesstracker.model.Cardio;
+import is.hi.hbv601.fitnesstracker.model.User;
+import is.hi.hbv601.fitnesstracker.network.NetworkClient;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class AddCardioActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    private NetworkClient networkClient;
 
     String cardioType;
 
@@ -42,27 +63,101 @@ public class AddCardioActivity extends AppCompatActivity implements AdapterView.
         CardioSpinner.setAdapter(myAdapter);
 
         final Button mSaveCardioButton = findViewById(R.id.save_cardio);
-
-        /**
-         * Button
-         * Navigates to main activity page
-         */
+        
         mSaveCardioButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
+                        if (isNetworkAvailable()) {
+                            toggleRefresh();
+                            networkClient = new NetworkClient();
+                            final int duration = Integer.parseInt(mCardioDuration.getText().toString());
+                            final Date date = new Date();
+                            final String type = "eh";
+                            final int topspeed = 1;
+                            final int distance = 1;
+                            // Create Json user
+                            final User loggedInUser = new User("Tommi", "Jenni");
+                            JSONObject jsonUser = new JSONObject();
+                            try {
+                                jsonUser.put("userName", loggedInUser.getUserName());
+                                jsonUser.put("password", loggedInUser.getPassword());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            JSONObject jsonCardio = new JSONObject();
+                            // User user, int duration, Date date, String type, int weight, int times
+                            final Cardio cardio = new Cardio(loggedInUser, duration, date, type, topspeed, distance, null);
+                            try {
+                                jsonCardio.put("user", jsonUser);
+                                jsonCardio.put("duration", duration);
+                                jsonCardio.put("date", date);
+                                jsonCardio.put("type", type);
+                                jsonCardio.put("topspeed", topspeed);
+                                jsonCardio.put("distance", distance);
+                                jsonCardio.put("route", null);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            // Post addcardio information to server
+                            Call call = networkClient.post("exercise/addcardio", jsonUser.toString());
+                            call.enqueue(new Callback() {
+                                @Override
+                                public void onFailure(Call call, IOException e) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            toggleRefresh();
+                                        }
+                                    });
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            showAddCardioFailed();
+                                        }
+                                    });
+                                }
 
-                final int cardioDuration = Integer.parseInt(mCardioDuration.getText().toString());
-                final int cardioDistance = Integer.parseInt(mCardioDistance.getText().toString());
-
-                /* // TODO: Tengja við gagnagrunn þannig að æfingar saveist
-                Toast.makeText(getBaseContext(), "Saving Exercise...", Toast.LENGTH_SHORT).show();
-
-                MainActivity(v);
-                finish(); */
+                                @Override
+                                public void onResponse(Call call, Response response) throws IOException {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            toggleRefresh();
+                                        }
+                                    });
+                                    try {
+                                        String jsonData = response.body().string();
+                                        Log.v(TAG, jsonData);
+                                        // Login succesful
+                                        if (response.isSuccessful()) {
+                                            final ObjectMapper mapper = new ObjectMapper();
+                                            Map<String,Object> map;
+                                            map = mapper.readValue(jsonData, new TypeReference<HashMap<String,Object>>(){});
+                                            Log.v(TAG, map.toString());
+                                            // Bætir æfingu við listann á núverandi notenda.
+                                            // addStrength(jsonData);
+                                            loggedInUser.addExercise(cardio);
+                                            MainActivity(v);
+                                            finish();
+                                        } else {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    showAddCardioFailed();
+                                                }
+                                            });
+                                        }
+                                    } catch (IOException e) {
+                                        Log.e(TAG, "Exception caught: ", e);
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(getApplicationContext(), R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
-
-        });
-    }
 
     public void MainActivity(View v) {
         Intent i = new Intent(this, MainActivity.class);
@@ -79,5 +174,21 @@ public class AddCardioActivity extends AppCompatActivity implements AdapterView.
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    private void showAddCardioFailed() {
+        //mLoginFailed.setVisibility(View.VISIBLE);
+    }
+
+    private void toggleRefresh() {
+        //
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo!= null && networkInfo.isConnected()) isAvailable = true;
+        return isAvailable;
     }
 }
--- app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddStrengthActivity.java
+++ app/src/main/java/is/hi/hbv601/fitnesstracker/ui/activity/AddStrengthActivity.java
@@ -1,7 +1,11 @@
 package is.hi.hbv601.fitnesstracker.ui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,9 +16,34 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import is.hi.hbv601.fitnesstracker.R;
+import is.hi.hbv601.fitnesstracker.model.Strength;
+import is.hi.hbv601.fitnesstracker.model.User;
+import is.hi.hbv601.fitnesstracker.network.NetworkClient;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.Response;
 
 public class AddStrengthActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+    private static final String URL = "https://hugbo2-2020.herokuapp.com/";
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    private NetworkClient networkClient;
 
     String strengthType;
 
@@ -37,25 +66,107 @@ public class AddStrengthActivity extends AppCompatActivity implements AdapterVie
 
         final Button mSaveStrengthButton = findViewById(R.id.save_strength);
 
-        /**
-         * Button
-         * Navigates to main activity page
-         */
         mSaveStrengthButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
+                if (isNetworkAvailable()) {
+                    toggleRefresh();
+                    networkClient = new NetworkClient();
+                    final int duration = Integer.parseInt(mStrengthDuration.getText().toString());
+                    final Date date = new Date();
+                    final String type = "eh";
+                    final int weight = 1;
+                    final int times = 1;
+                    // Create Json user
+                    final User loggedInUser = new User("Tommi", "Jenni");
+                    JSONObject jsonUser = new JSONObject();
+                    try {
+                        jsonUser.put("userName", loggedInUser.getUserName());
+                        jsonUser.put("password", loggedInUser.getPassword());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JSONObject jsonStrength = new JSONObject();
+                    // User user, int duration, Date date, String type, int weight, int times
+                    final Strength strength = new Strength(loggedInUser, duration, date, type, weight, times);
+                    try {
+                        jsonStrength.put("user", jsonUser);
+                        jsonStrength.put("duration", duration);
+                        jsonStrength.put("date", date);
+                        jsonStrength.put("type", type);
+                        jsonStrength.put("weight", weight);
+                        jsonStrength.put("times", times);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    // Post addcardio information to server
+                    Call call = networkClient.post("exercise/addstrength", jsonUser.toString());
+                    call.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    toggleRefresh();
+                                }
+                            });
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    showAddStrengthFailed();
+                                }
+                            });
+                        }
 
-                final int strengthDuration = Integer.parseInt(mStrengthDuration.getText().toString());
-
-                /*// TODO: Tengja við gagnagrunn þannig að æfingar saveist
-                Toast.makeText(getBaseContext(), "Saving Exercise...", Toast.LENGTH_SHORT).show();
-
-                MainActivity(v);
-                finish(); */
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    toggleRefresh();
+                                }
+                            });
+                            try {
+                                String jsonData = response.body().string();
+                                Log.v(TAG, jsonData);
+                                // Login succesful
+                                if (response.isSuccessful()) {
+                                    final ObjectMapper mapper = new ObjectMapper();
+                                    Map<String,Object> map;
+                                    map = mapper.readValue(jsonData, new TypeReference<HashMap<String,Object>>(){});
+                                    Log.v(TAG, map.toString());
+                                    // Bætir æfingu við listann á núverandi notenda.
+                                    // addStrength(jsonData);
+                                    loggedInUser.addExercise(strength);
+                                    MainActivity(v);
+                                    finish();
+                                } else {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            showAddStrengthFailed();
+                                        }
+                                    });
+                                }
+                            } catch (IOException e) {
+                                Log.e(TAG, "Exception caught: ", e);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+                }
             }
-
         });
     }
+
+    public void addStrength(String jSONString) {
+        // TODO
+        // Deserialize strength
+        // add to exercise list
+    }
+
+
     public void MainActivity(View v) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
@@ -71,5 +182,22 @@ public class AddStrengthActivity extends AppCompatActivity implements AdapterVie
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    
+    private void showAddStrengthFailed() {
+        //mLoginFailed.setVisibility(View.VISIBLE);
+    }
+
+    private void toggleRefresh() {
+        //
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo!= null && networkInfo.isConnected()) isAvailable = true;
+        return isAvailable;
     }
 }
